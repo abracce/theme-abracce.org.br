3ea587377cc885c877140ad18557e55f030805a2
Fernando Moreira <nandomoreira.me@gmail.com>
2017-04-10 18:43:20 -0300

    Updated footer

--- footer.php
+++ footer.php
@@ -4,48 +4,53 @@
  */
 ?>
 
-		<div class="clearfix"></div><!-- /clearfix -->
+    <div class="clearfix"></div><!-- /clearfix -->
 
-		<div class="gototop">
-			<a href="#"><i class="icon icon-chevron-up"></i></a>
-		</div>
+    <div class="gototop">
+      <a href="#"><i class="icon icon-chevron-up"></i></a>
+    </div>
 
-		<!-- .footer -->
-		<footer class="footer">
-			<span class="line top"></span>
+    <!-- .footer -->
+    <footer class="footer">
+      <span class="line top"></span>
 
-			<!-- .main-container -->
-			<div class="main-container">
+      <!-- .main-container -->
+      <div class="main-container">
 
-				<div class="row">
-					<div class="col-md-6">
-						<p class="text-muted">&copy;2014 <?php bloginfo( 'name' ); ?>. Desevolvido por <a href="http://nandomoreira.me/" target="_blank">Nando Moreira</a>.</p>
-					</div>
+        <div class="row">
+          <div class="col-md-8">
+            <p class="text-muted">
+              &copy;2014 <?php bloginfo( 'name' ); ?>. Desevolvido por <a href="http://nandomoreira.me/" target="_blank">Nando Moreira</a>.
+              <a href="https://app.umbler.com/u/mzkqd8j2" style="display:inline-block; margin-left: 15pxs;">
+                <img src="https://static.umbler.com/brand/umbler-badges/umbler-badge-hostedby-dark-bg-mono.png" width="60px" alt="Hosted by Umbler" style="display:inline-block">
+              </a>
+            </p>
+          </div>
 
-					<div class="col-md-6">
-						<?php
-							wp_nav_menu(
-								array(
-									'theme_location' => 'footer-menu',
-									'depth'          => 2,
-									'container'      => false,
-									'menu_class'     => 'nav-footer list-inline pull-right',
-									'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
-									'walker'         => new Odin_Bootstrap_Nav_Walker()
-								)
-							);
-						?>
-					</div>
-				</div>
+          <div class="col-md-4">
+            <?php
+              wp_nav_menu(
+                array(
+                  'theme_location' => 'footer-menu',
+                  'depth'          => 2,
+                  'container'      => false,
+                  'menu_class'     => 'nav-footer list-inline pull-right',
+                  'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
+                  'walker'         => new Odin_Bootstrap_Nav_Walker()
+                )
+              );
+            ?>
+          </div>
+        </div>
 
-			</div>
-			<!-- /.main-container -->
+      </div>
+      <!-- /.main-container -->
 
-			<span class="line bottom"></span>
-		</footer>
-		<!-- /.footer -->
+      <span class="line bottom"></span>
+    </footer>
+    <!-- /.footer -->
 
-		<div class="clearfix"></div><!-- /clearfix -->
+    <div class="clearfix"></div><!-- /clearfix -->
     </div>
     <!-- /#main -->
 
